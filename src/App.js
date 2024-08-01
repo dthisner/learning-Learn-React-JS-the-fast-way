be686aa0e9f44f96dc4ce63335b50ca1acb4bb0b
dthisner <dthisner@pm.me>
2024-08-01 17:16:12 +0200

    lesson 14

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>Welcome</h1>
       </header>
+      <footer className='footer'>
+        <p className='footer'>Footer content goes here</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Styles.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img className='logo' src='logo.png' alt='logo-moviedux' />
+            <h2 className='app-subtitle'>Welcome</h2>
+        </div>
+    )
+
+}
