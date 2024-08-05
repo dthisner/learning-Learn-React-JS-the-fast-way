519d270a74b1ceaaf037a9a0a07eb809592693ee
dthisner <dthisner@pm.me>
2024-08-05 18:10:46 +0200

    at lesson 32

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -13,6 +13,19 @@ export default function MoviesGrid() {
   }, [])
 
   return (
-    <div>Movie Grid {movies.length}</div>
+    <div className="movies-grid">
+      {
+        movies.map(movie => (
+          <div key={movie.id} className="movie-card">
+            < img src={`images/${movie.image}`} alt={movie.title} />
+            <div className="movie-card-info">
+              <h3 className="movie-card-title">{movie.title}</h3>
+              <p className="movie-card-genre">{movie.genre}</p>
+              <p className="movie-card-raiting">{movie.raiting}</p>
+            </div>
+          </div >
+        ))
+      }
+    </div >
   );
 }
